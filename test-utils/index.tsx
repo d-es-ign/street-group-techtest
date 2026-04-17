1101cccaf581f79be061d701eac8a94ae4d96b04
Jasper van Es <jasper.vanes@awaze.com>
2026-04-17 11:10:22 +0100

    setup shared query infra

--- test-utils/index.tsx
+++ test-utils/index.tsx
@@ -2,17 +2,22 @@ import { render } from "@testing-library/react-native";
 import { ReactElement, ReactNode } from "react";
 import { ThemeProvider } from "styled-components/native";
 
+import { createQueryWrapper } from "@/domains/shared/test-utils/query-wrapper";
 import { appTheme } from "@/theme/theme";
 
-const Providers = ({ children }: { children: ReactNode }) => {
-  return <ThemeProvider theme={appTheme}>{children}</ThemeProvider>;
-};
-
 const customRender = (ui: ReactElement) => {
+  const QueryWrapper = createQueryWrapper();
+
   return render(ui, {
-    wrapper: ({ children }) => <Providers>{children}</Providers>,
+    wrapper: ({ children }: { children: ReactNode }) => {
+      return (
+        <QueryWrapper>
+          <ThemeProvider theme={appTheme}>{children}</ThemeProvider>
+        </QueryWrapper>
+      );
+    },
   });
 };
 
 export * from "@testing-library/react-native";
-export { customRender as render };
+export { createQueryWrapper, customRender as render };
